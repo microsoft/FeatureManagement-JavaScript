4e2f5b0e8f317aa149b272563e552ec435437532
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2024-09-18 13:15:12 +0800

    update ci pipeline & script

--- scripts/build-and-pack.sh
+++ scripts/build-and-pack.sh
@@ -3,44 +3,36 @@
 # Stop on error.
 set -e
 
-# Get the directory of the script.
 SCRIPT_DIR=$(dirname $(readlink -f $0))
-
-# Get the directory of the project.
 PROJECT_BASE_DIR=$(dirname $SCRIPT_DIR)
-
-# Define the SDK directory.
 SDK_DIR="$PROJECT_BASE_DIR/sdk"
 
-# Check if a package directory argument is provided.
-if [ -z "$1" ]; then
-  echo "Please specify a package directory."
-  exit 1
-fi
+PACKAGE="feature-management"
+PACKAGE_DIR="$SDK_DIR/$PACKAGE"
 
-# The directory of the package to build.
-PACKAGE_DIR="$SDK_DIR/$1"
+echo "Building package $PACKAGE in $PACKAGE_DIR"
+cd "$PACKAGE_DIR"
 
-if [ -d "$PACKAGE_DIR" ]; then
-  echo "Building package in $PACKAGE_DIR"
-  
-  # Change to the package directory.
-  cd "$PACKAGE_DIR"
-  
-  # Install dependencies, build, and test.
-  echo "npm clean install in $PACKAGE_DIR"
-  npm ci
+echo "npm clean install in $PACKAGE_DIR"
+npm ci
 
-  echo "npm run build in $PACKAGE_DIR"
-  npm run build
+echo "npm run build in $PACKAGE_DIR"
+npm run build
 
-  echo "npm run test in $PACKAGE_DIR"
-  npm run test
+echo "npm run test in $PACKAGE_DIR"
+npm run test
 
-  # Create a tarball.
-  echo "npm pack in $PACKAGE_DIR"
-  npm pack
-else
-  echo "The specified package directory does not exist."
-  exit 1
-fi
+echo "npm pack in $PACKAGE_DIR"
+npm pack
+
+PACKAGE="feature-management-applicationinsights-browser"
+PACKAGE_DIR="$SDK_DIR/$PACKAGE"
+
+echo "Building package $PACKAGE in $PACKAGE_DIR"
+cd "$PACKAGE_DIR"
+
+echo "npm run build in $PACKAGE_DIR"
+npm run build
+
+echo "npm pack in $PACKAGE_DIR"
+npm pack
