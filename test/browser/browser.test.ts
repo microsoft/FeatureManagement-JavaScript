ca232d239e164d07873f67f73949d063ed238848
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2024-09-25 14:03:07 +0800

    update browser test

--- test/browser/browser.test.ts
+++ test/browser/browser.test.ts
@@ -1,4 +1,4 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import chai from "chai";
 import path from "path";
 
@@ -7,7 +7,6 @@ test("Testcase can pass in browser environment", async ({ page }) => {
   const filePath = path.join(__dirname, "index.html");
 
   let hasPageError = false;
-
   page.on("pageerror", (err) => {
     hasPageError = true;
     console.log(`Page Error: ${err.message}`);
@@ -15,8 +14,15 @@ test("Testcase can pass in browser environment", async ({ page }) => {
 
   await page.goto(`file:${filePath}`);
 
-  const failures = await page.evaluate(() => (window as any).mochaFailures);
+  await page.waitForTimeout(10000);
 
-  chai.expect(failures).to.equal(0);
   chai.expect(hasPageError).to.be.false;
+
+  const failures = await page.locator('li.failures em').innerText();
+
+  chai.expect(failures).to.equal('0');
+
+  const passes = await page.locator('li.passes em').innerText();
+
+  chai.expect(passes).to.equal('9');
 });
