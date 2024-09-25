7945903a96f26f246311b960710a0977c12f96c2
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2024-09-25 16:43:54 +0800

    export version

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@
 export { FeatureManager } from "./featureManager.js";
 export { ConfigurationMapFeatureFlagProvider, ConfigurationObjectFeatureFlagProvider, IFeatureFlagProvider } from "./featureProvider.js";
 export { IFeatureFilter } from "./filter/FeatureFilter.js";
+export { VERSION } from "./version.js";
