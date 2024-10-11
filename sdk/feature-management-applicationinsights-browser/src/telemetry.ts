e546a4772e2d0db2a4fbf9e45814da72d3c583da
Zhiyuan Liang <141655842+zhiyuanliang-ms@users.noreply.github.com>
2024-10-11 11:08:56 +0800

    Add @microsoft/feature-management-applicationinsights-browser package (#39)
    * support app insights for browser
    
    * test ci
    
    * remove unrelated external dependency
    
    * update ci pipeline & script
    
    * update ci
    
    * update ci
    
    * update testcase
    
    * support telemetry metadata
    
    * correct comment
    
    * ensure targetingId is string
    
    * handle possible null

---
+++ sdk/feature-management-applicationinsights-browser/src/telemetry.ts
@@ -0,0 +1,52 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { EvaluationResult } from "@microsoft/feature-management";
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+import { IEventTelemetry } from "@microsoft/applicationinsights-web";
+
+/**
+ * Creates a telemetry publisher that sends feature evaluation events to Application Insights.
+ * @param client The Application Insights telemetry client.
+ * @returns A callback function that takes an evaluation result and tracks an event with the evaluation details.
+ */
+export function createTelemetryPublisher(client: ApplicationInsights): (event: EvaluationResult) => void {
+    return (event: EvaluationResult) => {
+        const eventProperties = {
+            "FeatureName": event.feature?.id,
+            "Enabled": event.enabled.toString(),
+            // Ensure targetingId is string so that it will be placed in customDimensions
+            "TargetingId": event.targetingId?.toString(),
+            "Variant": event.variant?.name,
+            "VariantAssignmentReason": event.variantAssignmentReason,
+        };
+
+        const metadata = event.feature?.telemetry?.metadata;
+        if (metadata) {
+            for (const key in metadata) {
+                if (!(key in eventProperties)) {
+                    eventProperties[key] = metadata[key];
+                }
+            }
+        }
+
+        client.trackEvent({ name: "FeatureEvaluation" }, eventProperties);
+    };
+}
+
+/**
+ * Tracks a custom event using Application Insights, ensuring that the "TargetingId"
+ * is included in the custom properties. If the "TargetingId" already exists in
+ * the provided custom properties, it will be overwritten.
+ *
+ * @param client The Application Insights client instance used to track the event.
+ * @param targetingId The unique targeting identifier that will be included in the custom properties.
+ * @param event The event telemetry object to be tracked, containing event details.
+ * @param customProperties (Optional) Additional properties to include in the event telemetry.
+ */
+export function trackEvent(client: ApplicationInsights, targetingId: string, event: IEventTelemetry, customProperties?: {[key: string]: any}): void {
+    const properties = customProperties ? { ...customProperties } : {};
+    // Ensure targetingId is string so that it will be placed in customDimensions
+    properties["TargetingId"] = targetingId?.toString();
+    client.trackEvent(event, properties);
+}
