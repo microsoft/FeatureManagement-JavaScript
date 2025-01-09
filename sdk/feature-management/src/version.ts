ab4b401ebb5a77f63a2adcf4153b6801047f37e6
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2025-01-09 12:49:24 +0800

    version bump v3 preview

--- sdk/feature-management/src/version.ts
+++ sdk/feature-management/src/version.ts
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export const VERSION = "2.0.0-preview.3";
+export const VERSION = "3.0.0-preview.1";
 export const EVALUATION_EVENT_VERSION = "1.0.0";
