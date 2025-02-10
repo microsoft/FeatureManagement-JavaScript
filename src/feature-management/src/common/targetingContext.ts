b9bcbc99c53ed4a040e7c0d8f20776b3d6a88c0f
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2025-02-10 18:36:46 +0800

    add comments

--- src/feature-management/src/common/targetingContext.ts
+++ src/feature-management/src/common/targetingContext.ts
@@ -1,9 +1,21 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+/**
+ * Contextual information that is required to perform a targeting evaluation.
+ */
 export interface ITargetingContext {
+    /**
+     * The user id that should be considered when evaluating if the context is being targeted.
+     */
     userId?: string;
+    /**
+     * The groups that should be considered when evaluating if the context is being targeted.
+     */
     groups?: string[];
 }
 
+/**
+ * Type definition for a function that, when invoked, returns the @see ITargetingContext for targeting evaluation.
+ */
 export type TargetingContextAccessor = () => ITargetingContext;
