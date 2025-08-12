224d72c9dabb7d3960d12dd270375a3095ec93a7
zhiyuanliang <zhiyuanliang@microsoft.com>
2025-08-12 09:58:52 +0800

    revert change

--- src/feature-management/src/filter/timeWindowFilter.ts
+++ src/feature-management/src/filter/timeWindowFilter.ts
@@ -33,7 +33,7 @@ export type RecurrenceParameters = {
 };
 
 export class TimeWindowFilter implements IFeatureFilter {
-    name: string = "Microsoft.TimeWindow";
+    readonly name: string = "Microsoft.TimeWindow";
 
     evaluate(context: TimeWindowFilterEvaluationContext): boolean {
         const {featureName, parameters} = context;
