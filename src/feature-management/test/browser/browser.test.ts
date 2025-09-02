4c042e0a4ca6eae114ee13eea04d407a304a7faa
zhiyuanliang <zhiyuanliang@microsoft.com>
2025-09-02 23:25:46 +0800

    fix test

--- src/feature-management/test/browser/browser.test.ts
+++ src/feature-management/test/browser/browser.test.ts
@@ -1,10 +1,6 @@
 import { test } from "@playwright/test";
 import * as chai from "chai";
 import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 test("Testcase can pass in browser environment", async ({ page }) => {
 
