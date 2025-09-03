9341da161eac2b7332c17f96a01578ebc9db7a88
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2025-09-03 23:57:25 +0800

    update

--- src/feature-management/vitest.browser.config.ts
+++ src/feature-management/vitest.browser.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     browser: {
       enabled: true,
       provider: "playwright",
+      headless: true,
       instances: [
         { browser: "chromium" },
       ],
